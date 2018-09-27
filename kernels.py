41a3bd20da4cb3278bac8a5f12cd7d4aafe9ba83
Natalie Axelsson <nataxe@kth.se>
2018-09-27 12:59:53 +0200

    Make apply_kernel not modify argument image

--- kernels.py
+++ kernels.py
@@ -1,15 +1,17 @@
 import numpy as np
+from image import new_image
 from scipy.signal import convolve2d
 
 
 def apply_kernel(image, kernel):
-	image[:, :, 0] = convolve2d(
+	im = new_image(image.shape[1], image.shape[0])
+	im[:, :, 0] = convolve2d(
 		image[:, :, 0], kernel, mode="same", boundary='symm')
-	image[:, :, 1] = convolve2d(
+	im[:, :, 1] = convolve2d(
 		image[:, :, 1], kernel, mode="same", boundary='symm')
-	image[:, :, 2] = convolve2d(
+	im[:, :, 2] = convolve2d(
 		image[:, :, 2], kernel, mode="same", boundary='symm')
-	return image
+	return im
 
 
 def box_blur(size):
