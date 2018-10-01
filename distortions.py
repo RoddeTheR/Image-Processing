a7f7fc6e13ac65e350db1a67d1325d83c596817d
Natalie Axelsson <nataxe@kth.se>
2018-10-01 23:27:57 +0200

    Added wavy distortion

--- distortions.py
+++ distortions.py
@@ -7,3 +7,12 @@ def extend_border(image, pad_width):
 
 def unpad(image, pad_width):
     return image[pad_width:-pad_width, pad_width:-pad_width, :]
+
+
+def wavy(image, amplitude, frequency=1, phase=0):
+	y_dim, x_dim, _ = image.shape
+	x = np.arange(x_dim)
+	y = np.arange(y_dim)
+	displacement = (amplitude * np.sin(frequency * (x - phase) / x_dim * 2 * np.pi)).astype(int)
+	yy = np.clip(y[:, None] + displacement, 0, y_dim - 1)
+	return image[yy, x]
